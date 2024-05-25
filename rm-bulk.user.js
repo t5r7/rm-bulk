510e13ba68337737081689fcc6d9188fe6ab7eb3
Thomas R <git@tomr.me>
2024-05-25 01:08:58 +0100

    bump version for dav's tweak (will make extension actually pull change)

--- rm-bulk.user.js
+++ rm-bulk.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Railmiles Bulk Updater (Unofficial)
 // @namespace    https://tomr.me
-// @version      1.0.4
+// @version      1.0.5
 // @description  Add the ability to bulk update journeys logged with Railmiles.me
 // @author       TomR.me
 // @match        https://my.railmiles.me/*
